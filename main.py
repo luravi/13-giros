ac5ad43566ba1bfe1f31894338f48ce43c9bc6f6
LURAVI <61315321+luravi@users.noreply.github.com>
2024-08-23 12:24:54 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 def on_forever():
-    # Giro a la izquierda
-    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)  # Motor izquierdo adelante
-    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 50) # Motor derecho atrás
-    basic.pause(1000)  # Pausa 1 segundo
-    maqueen.motor_stop(maqueen.Motors.ALL)  # Detener ambos motores
-    basic.pause(500)  # Pausa 0.5 segundos
-
-    # Giro a la derecha
-    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 50) # Motor izquierdo atrás
-    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)  # Motor derecho adelante
-    basic.pause(1000)  # Pausa 1 segundo
-    maqueen.motor_stop(maqueen.Motors.ALL)  # Detener ambos motores
-    basic.pause(500)  # Pausa 0.5 segundos
-
-# Ejecutar el ciclo indefinidamente
+    # Primera secuencia de movimiento
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+    basic.pause(1000)
+    # Primera parada de los motores
+    maqueen.motor_stop(maqueen.Motors.ALL)
+    basic.pause(500)
+    # Segunda secuencia de movimiento
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    basic.pause(1000)
+    # Segunda parada de los motores
+    maqueen.motor_stop(maqueen.Motors.ALL)
+    basic.pause(500)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,25 +1,16 @@
-// Pausa 0.5 segundos
-// Ejecutar el ciclo indefinidamente
 basic.forever(function () {
-    // Giro a la izquierda
+    // Primera secuencia de movimiento
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    // Motor izquierdo adelante
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
-    // Motor derecho atrás
     basic.pause(1000)
-    // Pausa 1 segundo
+    // Primera parada de los motores
     maqueen.motorStop(maqueen.Motors.All)
-    // Detener ambos motores
     basic.pause(500)
-    // Pausa 0.5 segundos
-    // Giro a la derecha
+    // Segunda secuencia de movimiento
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
-    // Motor izquierdo atrás
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    // Motor derecho adelante
     basic.pause(1000)
-    // Pausa 1 segundo
+    // Segunda parada de los motores
     maqueen.motorStop(maqueen.Motors.All)
-    // Detener ambos motores
     basic.pause(500)
 })
